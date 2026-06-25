d36c0d2b685531a900138cf3d11c92889996a725
KiladzeGiga <94072036+KiladzeGiga@users.noreply.github.com>
2026-06-25 21:48:15 +0400

    add ingress to infra/prod

---
+++ infra/live/prod/ingress.tf
@@ -0,0 +1,10 @@
+resource "azurerm_public_ip" "ingress" {
+  name                = "pip-azproj-ingress"
+  resource_group_name = azurerm_resource_group.prod.name
+  location            = azurerm_resource_group.prod.location
+
+  allocation_method = "Static"
+  sku               = "Standard"
+
+  tags = local.tags
+}
