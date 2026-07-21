3317c043e3ac29de69d68cc747070185c2388ba6
KiladzeGiga <94072036+KiladzeGiga@users.noreply.github.com>
2026-07-22 00:10:15 +0400

    infra: enable key vault secrets provider

--- infra/live/prod/aks.tf
+++ infra/live/prod/aks.tf
@@ -19,6 +19,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
   oms_agent {
     log_analytics_workspace_id = azurerm_log_analytics_workspace.main.id
   }
+  
+  key_vault_secrets_provider {
+    secret_rotation_enabled = true
+  }
 
   default_node_pool {
     name       = "system"
